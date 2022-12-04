d58e70c6ab015064af5e2d93804cbbd2a6469963
pawan2348 <96902464+pawan2348@users.noreply.github.com>
2022-12-04 23:53:04 +0530

    alias 2

--- aws.tf
+++ aws.tf
@@ -4,28 +4,15 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-provider "aws" {
-  access_key = "AKIASRYPUS3KWPXKP4NJ"
-  secret_key = "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"
-  region     = "us-east-1"
-  alias="us"
 
-}
 
 resource "aws_instance" "ap" {
 ami="ami-069d9fecd19e7ed40"
 instance_type="t2.micro"
 tags={
-Name="indias"
+Name="india1"
 }
 }
 
-resource "aws_instance" "us" {
-ami="ami-05723c3b9cf4bf4ff"
-instance_type="t2.micro"
-provider=aws.us
-tags={
-Name="US"
-}
-}
+
 
